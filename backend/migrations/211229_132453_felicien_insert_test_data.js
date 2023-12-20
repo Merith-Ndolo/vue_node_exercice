b671469c0f1a1d2e410c0f04ec1a20550d4251dc
Merith-Pivot <merith@pivotandco.com>
2023-12-20 14:43:59 +0100

    feat: update migrations files

--- backend/migrations/211229_132453_felicien_insert_test_data.js
+++ backend/migrations/211229_132453_felicien_insert_test_data.js
@@ -8,14 +8,14 @@ export default {
         ('${uuid()}', 'Example Event 2');`
     );
     await q.sequelize.query(
-      `INSERT INTO Users (id, phone_number, first_name, last_name, last_connected, event_id)
+      `INSERT INTO Users (id, phone_number, first_name, last_name, last_connected, event_id, registration_number)
         SELECT '${uuid()}', '+33612345678', 'John', 'Doe', NULL, id
-        FROM Events WHERE name='Example Event 1';`
+        FROM Events WHERE name='Example Event 1', 12c2f057-4808-4fbf-af22-024cb68bc2e9;`
     );
     await q.sequelize.query(
-      `INSERT INTO Users (id, phone_number, first_name, last_name, last_connected, event_id)
+      `INSERT INTO Users (id, phone_number, first_name, last_name, last_connected, event_id, registration_number)
         SELECT '${uuid()}', '+33600000000', 'Jean', 'Dupont', NULL, id
-        FROM Events WHERE name='Example Event 2';`
+        FROM Events WHERE name='Example Event 2', 23c2f057-4808-4fbf-af22-024cb68bc2e9;`
     );
   },
   down: async () => {},
